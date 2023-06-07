bd821160d9af7c92d5d25da58c5938717b2085bb
CAILLOT Cyprien <cyprien.caillot@fixstudio.com>
2023-06-07 17:04:01 +0200

    update file structure

--- quad_pyblish_plugins.py
+++ quad_pyblish_plugins.py
@@ -5,8 +5,7 @@ from openpype.modules import IPluginPaths
 class QuadPyblishPlugins(IPluginPaths):
     name = "quadpyblish"
 
-    @staticmethod
-    def get_plugin_paths():
+    def get_plugin_paths(self):
         current_dir = os.path.dirname(os.path.abspath(__file__))
         return {
             "publish": [os.path.join(current_dir, "plugins", "publish")]
