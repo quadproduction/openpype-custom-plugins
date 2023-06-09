10ac9ac3a74deb6fae14398ca98150c99a8a09f9
Thomas Fricard <tfricard@ws114.prs.vfx.int>
2023-06-09 11:40:56 +0200

    add maya in hosts for ApplyLook class

--- quad_pyblish_module/plugins/maya/load/apply_look.py
+++ quad_pyblish_module/plugins/maya/load/apply_look.py
@@ -19,6 +19,7 @@ log = logging.getLogger(__name__)
 class ApplyLook(openpype.hosts.maya.api.plugin.ReferenceLoader):
     families = ["look"]
     representations = ["ma"]
+    hosts = ["maya"]
 
     label = "FixLookLoader"
     order = -10
