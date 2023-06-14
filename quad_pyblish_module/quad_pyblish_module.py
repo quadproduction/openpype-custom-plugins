02d0db37cf78d7c5c00ecc7a3d7751b798503186
Thomas Fricard <tfricard@ws114.prs.vfx.int>
2023-06-14 14:28:55 +0200

    add class that allows to add custom settings out of OP repo

--- quad_pyblish_module/quad_pyblish_module.py
+++ quad_pyblish_module/quad_pyblish_module.py
@@ -1,6 +1,10 @@
 import os
 import glob
-from openpype.modules import OpenPypeModule, IPluginPaths
+from openpype.modules import (
+    OpenPypeModule,
+    IPluginPaths,
+    JsonFilesSettingsDef
+)
 
 
 class QuadPyblishModule(OpenPypeModule, IPluginPaths):
@@ -17,7 +21,12 @@ class QuadPyblishModule(OpenPypeModule, IPluginPaths):
     def get_plugin_paths(self):
         """Implementation of abstract method for `IPluginPaths`."""
         # Regex to get all plugins from the plugins directory
-        current_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'plugins', '**', '*.py')
+        current_dir = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            'plugins',
+            '**',
+            '*.py'
+        )
 
         # Initialize all plugins which can be supported
         plugins_dict = {
@@ -32,9 +41,33 @@ class QuadPyblishModule(OpenPypeModule, IPluginPaths):
             if '__init__.py' in filepath:
                 continue
 
-            plugin_type = next((key for key in plugins_dict.keys() if key in filepath), None)
+            plugin_type = next(
+                (key for key in plugins_dict.keys() if key in filepath),
+                None
+            )
 
             if os.path.dirname(filepath) not in plugins_dict[plugin_type]:
                 plugins_dict[plugin_type].append(os.path.dirname(filepath))
 
         return plugins_dict
+
+
+class AddonSettingsDef(JsonFilesSettingsDef):
+    # This will add prefixes to every schema and template from `schemas`
+    #   subfolder.
+    # - it is not required to fill the prefix but it is highly
+    #   recommended as schemas and templates may have name clashes across
+    #   multiple addons
+    # - it is also recommended that prefix has addon name in it
+    schema_prefix = "fix_custom_settings"
+
+    def get_settings_root_path(self):
+        """Implemented abstract class of JsonFilesSettingsDef.
+
+        Return directory path where json files defying addon settings are
+        located.
+        """
+        return os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "settings"
+        )
