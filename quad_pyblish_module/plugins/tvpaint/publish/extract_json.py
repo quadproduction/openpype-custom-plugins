8b64935764bf68bd584bb9b804d6f3c1fd6306f2
clement hector <clement.hector@fixstudio.com>
2023-06-16 16:19:18 +0200

    adapt extract json module to context

--- quad_pyblish_module/plugins/tvpaint/publish/extract_json.py
+++ quad_pyblish_module/plugins/tvpaint/publish/extract_json.py
@@ -12,7 +12,7 @@ from openpype.settings import get_project_settings
 class ExtractJson(pyblish.api.ContextPlugin):
     """ Extract a JSON file and add it to the instance representation.
     """
-    order = pyblish.api.ExtractorOrder
+    order = pyblish.api.ExtractorOrder + 0.01
     label = "Extract JSON"
     hosts = ["tvpaint"]
 
@@ -23,14 +23,16 @@ class ExtractJson(pyblish.api.ContextPlugin):
         'ExtractJson']['enabled']
 
     def process(self, context):
-        # Save to staging dir
-        output_dir = context.data.get("stagingDir")
-        if not output_dir:
-            # Create temp folder if staging dir is not set
-            output_dir = (
-                tempfile.mkdtemp(prefix="tvpaint_render_")
-            ).replace("\\", "/")
-            context.data["stagingDir"] = output_dir
+
+        # Create temp folder
+        output_dir = (
+            tempfile.mkdtemp(prefix="tvpaint_render_")
+        ).replace("\\", "/")
+
+        context.data["tvpaint_export_json"] = {"stagingDir": output_dir}
+
+        context_data = context.data.get("tvpaint_export_json")
+        self.log.info(context_data)
 
         self.log.info('Extract Json')
         # TODO: george script in list
@@ -42,21 +44,21 @@ class ExtractJson(pyblish.api.ContextPlugin):
         lib.execute_george_through_file(george_script_lines)
 
         raw_json_path = os.path.join(output_dir, 'tvpaint.json')
-        context.data['json_output_dir'] = output_dir
-        context.data['raw_json_data_path'] = raw_json_path
+        context_data['json_output_dir'] = output_dir
+        context_data['raw_json_data_path'] = raw_json_path
 
         with open(raw_json_path) as tvpaint_json:
             tvpaint_data = json.load(tvpaint_json)
 
-        context.data['tvpaint_layers_data'] = tvpaint_data['project']['clip']['layers']  # noqa
+        context_data['tvpaint_layers_data'] = tvpaint_data['project']['clip']['layers']  # noqa
         instance_layer = [
-            layer['name'] for layer in context.data.get('layers')
+            layer['name'] for layer in context.data.get('layersData')
         ]
 
-        if context.data.get('instance_layers'):
-            context.data['instance_layers'].extend(instance_layer)
+        if context_data.get('instance_layers'):
+            context_data['instance_layers'].extend(instance_layer)
         else:
-            context.data['instance_layers'] = instance_layer
+            context_data['instance_layers'] = instance_layer
 
         tvpaint_data['project']['clip']['layers'] = []
         op_json_filename = 'openpype.json'
@@ -71,5 +73,11 @@ class ExtractJson(pyblish.api.ContextPlugin):
             "stagingDir": output_dir,
             "tags": ["json_data"]
         }
-        context.data["representations"].append(json_repres)
+        if context_data.get("representations"):
+            context_data["representations"].append(json_repres)
+        else:
+            context_data["representations"] = [json_repres]
+
+        self.log.info(context_data)
+
         self.log.debug("Add json representation: {}".format(json_repres))
