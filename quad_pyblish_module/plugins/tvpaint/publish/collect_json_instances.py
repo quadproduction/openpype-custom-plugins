aaf0cc61b2d0f386741a7542d97499b4bbf32c08
clement hector <clement.hector@fixstudio.com>
2023-06-22 09:51:48 +0200

    create Creator and Collector for json instances

---
+++ quad_pyblish_module/plugins/tvpaint/publish/collect_json_instances.py
@@ -0,0 +1,30 @@
+import copy
+import pyblish.api
+from openpype.lib import prepare_template_data
+
+
+class CollectJsonInstances(pyblish.api.InstancePlugin):
+    label = "Collect Json Instances"
+    order = pyblish.api.CollectorOrder - 0.41
+    hosts = ["tvpaint"]
+    families = ["json"]
+
+    def process(self, instance):
+        context = instance.context
+        creator_identifier = instance.data["creator_identifier"]
+        self._collect_data_for_json(instance)
+
+        subset_name = instance.data["subset"]
+        instance.data["name"] = subset_name
+        instance.data["label"] = "{} [{}-{}]".format(
+            subset_name,
+            context.data["sceneMarkIn"] + 1,
+            context.data["sceneMarkOut"] + 1
+        )
+
+    def _collect_data_for_json(self, instance):
+        instance.data['families'].append('json')
+        creator_attributes = instance.data['creator_attributes']
+        instance.data["layers"] = copy.deepcopy(
+            instance.context.data['layersData']
+        )
