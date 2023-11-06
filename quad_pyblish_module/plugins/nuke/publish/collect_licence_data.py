f1359182839f13e8420825f43e8a97e9c8fdbc01
ccaillot <cyprien.caillot@gmail.com>
2023-11-06 19:58:44 +0100

    Add check if user already have licence

--- quad_pyblish_module/plugins/nuke/publish/collect_licence_data.py
+++ quad_pyblish_module/plugins/nuke/publish/collect_licence_data.py
@@ -5,24 +5,26 @@ import os
 
 
 class CollectLicenceData(pyblish.api.InstancePlugin):
+
     """Collect Licence Data"""
 
     hosts = ["nuke"]
     label = "Collect Licence Data"
     order = pyblish.api.CollectorOrder + 0.4990
+    # TODO: avoid hardcoded path
+    rlm_path = "/prod/softprod/tools/linux/rlmutil"
 
-    def process(self, instance):
+    def get_available_licenses(self):
         """
         """
         # Define the command to run
-        # TODO: avoid hardcoded path
-        command = f'/prod/softprod/tools/linux/rlmutil rlmstat -c {os.environ["foundry_LICENSE"]} -avail'
+        avail_command = f'{self.rlm_path} rlmstat -c {os.environ["foundry_LICENSE"]} -avail'
 
         # Define the pattern for matching the number of available licenses for nuke_i
         pattern = r'nuke_i v\d+\.\d+ available: (\d+)'
 
         # Run the command and capture the output
-        result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
+        result = subprocess.run(avail_command, shell=True, stdout=subprocess.PIPE, text=True)
 
         # Use regex to find the match in the output
         match = re.search(pattern, result.stdout)
@@ -31,11 +33,40 @@ class CollectLicenceData(pyblish.api.InstancePlugin):
         if match:
             # Extract the number of available licenses
             available_licenses = int(match.group(1))
-            print(f"Available licenses for nuke_i: {available_licenses}")
         else:
-            print("No match found for nuke_i licenses.")
             return False
 
         if available_licenses == 0:
             return False
 
+        return True
+
+    def get_user_licenses(self):
+        # Define the pattern for matching user licenses for nuke_i
+        user_command = f'{self.rlm_path} rlmstat -c {os.environ["foundry_LICENSE"]} -u {os.environ["USER"]}'
+
+        user_pattern = r'nuke_i v\d+\.\d+: (\S+)@\S+ (\d+)/(\d+)'
+
+        # Run the command and capture the output
+        result = subprocess.run(user_command, shell=True, stdout=subprocess.PIPE, text=True)
+
+        # Use regex to find the match in the output
+        matches = re.findall(user_pattern, result.stdout)
+
+        # Check if any matches are found
+        if matches:
+            return True
+
+        return False
+
+    def process(self, instance):
+        # Check if user already have a licence
+        if self.get_user_licenses():
+            return True
+
+        # Check if user can get an available license
+        if self.get_available_licenses():
+            return True
+
+        # User can't publish
+        raise Exception("No available licence ! Can't publish")
