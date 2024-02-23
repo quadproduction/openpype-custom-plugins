65529e01c12e8632e034057477b7cb2884ae35f5
Guilhem Compain <gcompain@wizz.vfx.int>
2024-02-23 15:44:24 +0100

    #321: clean: correct typo error

---
+++ quad_pyblish_module/plugins/blender/publish/validate_frame_start.py
@@ -0,0 +1,44 @@
+from typing import List
+
+import bpy
+
+import pyblish.api
+
+import openpype.hosts.blender.api.action
+from openpype.pipeline.publish import ValidateContentsOrder
+from openpype.pipeline import (
+    PublishXmlValidationError,
+    OptionalPyblishPluginMixin,
+)
+
+
+class ValidateFrameStart(
+    OptionalPyblishPluginMixin,
+    pyblish.api.ContextPlugin):
+    """Camera must have a keyframe at frame 0.
+
+    Unreal shifts the first keyframe to frame 0. Forcing the camera to have
+    a keyframe at frame 0 will ensure that the animation will be the same
+    in Unreal and Blender.
+    """
+
+    order = ValidateContentsOrder
+    hosts = ["blender"]
+    families = ["*"]
+    label = "Frame Start"
+    actions = [openpype.hosts.blender.api.action.SelectInvalidAction]
+    optional = True
+
+    def process(self, context):
+        registered_frame_start = context.data.get('frameStart')
+        if not registered_frame_start:
+            raise RuntimeError(
+                "Can't retrieve frame start information from OpenPype."
+            )
+        scene_frame_start = bpy.context.scene.frame_start
+        print(registered_frame_start)
+        print(scene_frame_start)
+        if registered_frame_start != scene_frame_start:
+            raise RuntimeError(
+                f"Frame start for scene ({scene_frame_start} if different from OpenPype's frame start ({registered_frame_start}).)"
+            )
